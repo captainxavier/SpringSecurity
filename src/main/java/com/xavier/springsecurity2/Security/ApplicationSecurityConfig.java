241ca7177cc7f8efbe7d6a1d81926ec529a852d1
android-hela <osisxavier@gmail.com>
2021-06-05 00:27:21 +0300

    Added authorities Second instance

--- src/main/java/com/xavier/springsecurity2/Security/ApplicationSecurityConfig.java
+++ src/main/java/com/xavier/springsecurity2/Security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,6 +20,7 @@ import static com.xavier.springsecurity2.Security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -35,10 +37,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/","index","/css/*","/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(USER.name())
-                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(EMPLOYEE_WRITE.name())
-                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(EMPLOYEE_WRITE.name())
-                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(EMPLOYEE_WRITE.name())
-                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(),TRAINEE.name())
+//                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(EMPLOYEE_WRITE.name())
+//                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(EMPLOYEE_WRITE.name())
+//                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(EMPLOYEE_WRITE.name())
+//                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(),TRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
--- src/main/java/com/xavier/springsecurity2/controller/UserManagementController.java
+++ src/main/java/com/xavier/springsecurity2/controller/UserManagementController.java
@@ -2,6 +2,7 @@ package com.xavier.springsecurity2.controller;
 
 import com.xavier.springsecurity2.Utils.Utils;
 import com.xavier.springsecurity2.models.User;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,21 +12,24 @@ import java.util.List;
 public class UserManagementController {
 
     @GetMapping
-
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_TRAINEE')")
     public List<User> getAllUsers(){
         return Utils.getUsers();
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('employee:write')")
     public void registerNewUser(@RequestBody User user){
         System.out.println(user);
     }
 
     @DeleteMapping(path = "{userId}")
+    @PreAuthorize("hasAuthority('employee:write')")
     public void deleteUser(Integer userId){
         System.out.println(userId);
     }
     @PutMapping(path = "{userId}")
+    @PreAuthorize("hasAuthority('employee:write')")
     public void updateUser(Integer userId, User user){
         System.out.println(String.format("%s %s",userId, user));
     }
