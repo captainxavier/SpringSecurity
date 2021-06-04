cd78615b2fe0286b6ff1f93d0f598c927b899eb1
android-hela <osisxavier@gmail.com>
2021-06-04 23:07:51 +0300

    Initial commit

---
+++ src/main/java/com/xavier/springsecurity2/Utlits/Utils.java
@@ -0,0 +1,26 @@
+package com.xavier.springsecurity2.Utlits;
+
+import com.xavier.springsecurity2.models.User;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Utils {
+
+    private static  final List<User> users = Arrays.asList(
+            new User(1,"xavier"),
+            new User(2,"tom"),
+            new User(3,"asher"),
+            new User(4,"john"),
+            new User(5,"frank"),
+            new User(6,"paul"),
+            new User(7,"james"),
+            new User(8,"tracy"),
+            new User(9,"oscar")
+
+    );
+
+    public static List<User> getUsers() {
+        return users;
+    }
+}
---
+++ src/main/java/com/xavier/springsecurity2/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.xavier.springsecurity2.controller;
+
+import com.xavier.springsecurity2.Utlits.Utils;
+import com.xavier.springsecurity2.models.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/user")
+public class UserController {
+
+
+    @GetMapping(path = "{id}")
+    public User getPersonnel(@PathVariable("id") Integer id){
+        return Utils.getUsers()
+                .stream()
+                .filter(person -> id.equals(person.getId()))
+                .findFirst()
+                .orElseThrow(()->new IllegalStateException("Personnel with "+id+" not found."));
+    }
+}
