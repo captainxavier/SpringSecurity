23cf1668abff68a9b7c264799136627e87cc6fdf
android-hela <osisxavier@gmail.com>
2021-06-05 09:06:34 +0300

    Cross Site Request Forgery added(CSRF).

--- src/main/java/com/xavier/springsecurity2/Security/ApplicationSecurityConfig.java
+++ src/main/java/com/xavier/springsecurity2/Security/ApplicationSecurityConfig.java
@@ -34,9 +34,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-//                .and()
-                .csrf().disable()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .authorizeRequests()
                 .antMatchers("/","index","/css/*","/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(USER.name())
