a43ed04c4e9d7ff84d3e23f5f5779b23458f1632
android-hela <osisxavier@gmail.com>
2021-06-05 11:54:20 +0300

    Increase the valid life of token.

--- src/main/java/com/xavier/springsecurity2/Security/ApplicationSecurityConfig.java
+++ src/main/java/com/xavier/springsecurity2/Security/ApplicationSecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
 import static com.xavier.springsecurity2.Security.ApplicationUserPermission.EMPLOYEE_WRITE;
 import static com.xavier.springsecurity2.Security.ApplicationUserRole.*;
 
@@ -46,7 +49,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/products",true)
                 .and()
-                .rememberMe();
+                .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                .key("securekey");
     }
 
     @Override
